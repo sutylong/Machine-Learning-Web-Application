9d756acd9a1ec98dcaea8b6c1e2834d24bf170d7
sutylong <77848386+sutylong@users.noreply.github.com>
2022-02-19 00:56:12 +0700

    Update Main_interface.py

--- Main_interface.py
+++ Main_interface.py
@@ -4,15 +4,17 @@ import numpy as np
 from PIL import Image
 import plotly.express as px
 
-image = Image.open('D:\\Programming\\Code Workplace\\Python\\MyProjects\\Machine-Learning-Algorithms-Thuật-toán-Machine-Learning.png')
+image = Image.open('D:\\Programming\\Code Workplace\\Python\\MyProjects\\ML_Web_App\\image\\ml.png')
 img = st.image(image, use_column_width=True)
 
 st.sidebar.title("Choose Machine Learning Model")
 model_select = ['Home','Simple Linear Regression','Multivariable Linear Regression',
-                'Polynomial Regression','Decision Tree','Logistic Regression','SVM','Naive Bayes']
+                'Polynomial Regression','Decision Tree','Logistic Regression','SVM','Naive Bayes',
+                'Random Forest']
 
 choice = st.sidebar.selectbox('Select model...', model_select)
 
+###################################################################
 
 if choice == 'Simple Linear Regression':
     img.empty()
@@ -61,7 +63,10 @@ if choice == 'Simple Linear Regression':
         st.write('**Mean Absolute Error:** ', mean_ab_er)
         st.write('**Root Mean Square Error:**', rmse)
         st.write('**R-Squared value:**', rsquare)
-    
+
+
+###################################################################
+
 elif choice == 'Multivariable Linear Regression':
     img.empty()
     st.title("Welcome to Multivariable Linear Regression Application")
@@ -130,7 +135,18 @@ elif choice == 'Multivariable Linear Regression':
         st.write('**Mean Absolute Error:** ', mae)
         st.write('**Root Mean Square Error:**', rmse)
         st.write('**R-Squared value:**', rsquare)
-    
+
+###################################################################
+elif choice == 'Polynomial Regression':
+    img.empty()
+    st.info('Oops! Polynomial Regression is not available yet. Please come back later!')
+
+
+
+
+
+
+###################################################################
 
 elif choice == 'Decision Tree':
     img.empty()
@@ -142,14 +158,17 @@ elif choice == 'Decision Tree':
     """)
     st.write("##### Here is our dataset")   
 
-    from Decision_Tree import df, number_of_species, number_of_null
+    from Decision_Tree import df, number_of_species, number_of_null, df_new
+    from Decision_Tree import island_cate, sex_cate, dot_in_sex, sex_and_others_relation, check_sex
+    
+    if st.checkbox("Original dataset"):
+        st.write(df)
 
-    st.write(df)
-    st.write('''We can see that the data is not clean. There are **NA** values and some **noises**.
-    Let's explore the data !
-    ''')
+        st.write('''We can see that the data is not clean. There are **NA** values and some **noises**.
+        Let's explore the data !
+        ''')
 
-    if st.checkbox('Explore'):
+    if st.checkbox('Explore and Preprocessing data'):
         st.markdown('''
         First let's see how many species that we need to classify
         ''')
@@ -166,3 +185,144 @@ elif choice == 'Decision Tree':
         **flipper_length_mm**, **body_mass_g** we have 2 NA values. 
         """)
 
+        st.write("Let's see the dataset after removing **NA** values")
+        st.code('df_new = df.dropna()')
+        st.write(df_new)
+
+        st.write("Let's check if there are some noises remaining")
+        st.code("df_new['island'].unique()")
+        st.code("df_new['sex'].unique()")
+        st.write(island_cate)
+        st.write(sex_cate)
+
+        st.write("Oops! We have a noisy value in 'sex' columns. Let's check where it is")
+        st.code("df_new[df_new['sex'] == '.']")
+        st.write(dot_in_sex)
+
+        st.write("It seems that we need to replace this 'dot' with another gender")
+        st.write("""But first, let's see the relationship between other features and sex (just consider 
+        Gentoo species)
+        """)
+        st.code("df_new[df_new['species'] == 'Gentoo'].groupby('sex').describe().transpose()")
+        st.write(sex_and_others_relation)
+
+        st.write("""In this case, it seems to be female rather than male (just personally). So
+        let's replace it with **'FEMALE'**""")
+        st.code("df_new.at[336,'sex'] = 'FEMALE'") 
+        st.write("Let's check if it has changed")
+        st.write(check_sex.astype(str))
+
+        st.write("""HOORAY! It has been changed. Let's take a look at our after-processing dataset
+        """)
+
+    if st.checkbox("After-processing data"):
+        st.dataframe(df_new)
+    
+    from Decision_Tree import pair_fig, cat_fig
+    from Decision_Tree import X_test, y_test, df_test_set,X_train,y_train
+    from Decision_Tree import report_model
+
+    
+    if st.checkbox("Pair Plot"):
+        st.pyplot(pair_fig)
+
+    if st.checkbox("Categorical Plot"):
+        st.pyplot(cat_fig)
+
+    if st.checkbox("Test Set"):
+        st.dataframe(df_test_set)
+
+    if st.checkbox("Prediction and Evaluation"):
+        st.sidebar.subheader('Features Selection')
+        
+        with st.sidebar:
+            with st.form('Form1'):
+                island = st.selectbox('Island',
+                                    ['Torgersen','Biscoe','Dream'])
+                culmen_length = st.number_input('Culmen_length_mm',0.0,100.0, step= 1.0)
+                culmen_depth = st.number_input('Culmen_depth_mm',0.0,50.0, step= 1.0)
+                flipper_length = st.number_input('Flipper_length_mm',0.0,250.0, step= 1.0)
+                body_mass = st.number_input('Body_mass_g',0.0,7000.0, step= 1.0)     
+                sex = st.selectbox('Sex', ['FEMALE','MALE'])     
+
+                submitted = st.form_submit_button("Predict")
+
+        data = {'island': island,
+                'culmen_length_mm': culmen_length,
+                'culmen_depth_mm': culmen_depth,
+                'flipper_length_mm': flipper_length,
+                'body_mass_g': body_mass,
+                'sex': sex}
+
+        features = pd.DataFrame(data, index=[0])
+        
+        # Encode user_input
+        penguins = df_new.drop('species', axis=1)  # Use the cleaned dataframe, not the get_dummies one
+        user_input = pd.concat([features,penguins], axis=0)
+    
+        encode = ['island','sex']
+        for col in encode:
+            dummy = pd.get_dummies(user_input[col], prefix= col, drop_first= True)
+            user_input = pd.concat([user_input,dummy], axis=1)
+            del user_input[col]
+        user_input = user_input[:1]
+        
+        st.subheader('Your choice')
+        st.write(features)
+        st.write('After get_dummies')
+        st.write(user_input)
+
+        # Predict
+        from Decision_Tree import tuning_model, train_model, show_metrics, cfm_plot
+
+        st.subheader('Tuning Hyperparameter')
+        model = tuning_model()
+        model = train_model(model,X_train,y_train) 
+        y_pred, fea_importance = show_metrics(model,X_test)
+
+        if submitted:
+            prediction = model.predict(user_input)
+            prediction_proba = model.predict_proba(user_input)
+
+            st.write("#### Predicted: ")
+            st.write(prediction)
+            st.write("#### Prediction Probability")
+            st.write(prediction_proba)
+
+        if st.checkbox("Features Importances"):
+            st.dataframe(fea_importance)
+
+        if st.checkbox("Confusion Matrix"):
+            confu_matrix = cfm_plot(y_test,y_pred)
+            st.pyplot(confu_matrix)
+
+        if st.checkbox("Report model and plot tree"):
+            report_model(model)
+
+
+###################################################################
+elif choice == 'Logistic Regression':
+    img.empty()
+    st.info('Oops! Logistic Regression is not available yet. Please come back later!')
+
+
+
+###################################################################
+elif choice == 'SVM':
+    img.empty()
+    st.info('Oops! SVM is not available yet. Please come back later!')
+
+
+
+###################################################################
+elif choice == 'Naive Bayes':
+    img.empty()
+    st.info('Oops! Naive Bayes is not available yet. Please come back later!')
+
+
+
+
+###################################################################
+elif choice == 'Random Forest':
+    img.empty()
+    st.info('Oops! Random Forest is not available yet. Please come back later!')
